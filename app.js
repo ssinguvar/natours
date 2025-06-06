4b7de9a5577d1febef3f1ff55ff4a7ecf3c255f1
ssinguvar <ssinguvar@voxai.com>
2025-06-06 11:44:07 +0530

    added a print stmt

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
-
+console.log('hi');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
